40b6013ee5874e9de34112fb817368ded1f5c3a6
Iorwerth Owain Thomas <iorweth.owain.thomas@googlemail.com>
2019-02-14 23:53:00 +0000

    New draft -- rewrote derived types, blocked out new initialisation file, main routine file.

--- definedTypes.f90
+++ definedTypes.f90
@@ -3,31 +3,16 @@ module definedTypes
   use constants
   implicit none
 
-  type realVector
-     ! defines a real vector type
-     real(real12):: Comp(3), Norm
-  end type RealVector
+  type finegrid
+     real(real12)::kx,ky,kz,norm !defined as centre of cell
+     complex(real12)::omega2
+     integer::coarseMap !which coarse point to map to
+  end type finegrid
 
-  type complexVector
-     ! defines complex vector type
-     complex(kind=real12)::Comp(3),Norm
-  end type complexVector
+  type coarsegrid
+     real(real12)::kx,ky,kz,norm !defined as centre of cell
+     complex(real12)::omega2
+  end type coarsegrid
 
-  type atomicBasis
-     ! defines atomic basis
-     integer::nAtom
-     type(realVector)::unitClength ! length of unit cell
-     type(realVector),allocatable,dimension(:)::rBasis,kBasis
-  end type atomicBasis
-
-  type cellBasis
-     ! defines cells
-     integer::nCell!number of cells along one side
-     integer::nCelltot
-     type(realVector)::clusterLength
-     type(realVector),allocatable,dimension(:,:,:)::rBasis,kBasis
-     !note that kBasis denotes the bits that are in the *centre*
-     !of the cell as per Fig 3 of PRB
-  end type cellBasis
-
+  
 end module definedTypes
--- initialisation.f90
+++ initialisation.f90
@@ -5,73 +5,31 @@ module initialisation
 
 contains
 
-  subroutine getBasis(basis)
-    !dummy variables
-    type(atomicBasis),intent(inout)::basis
+  subroutine initGrid()
 
-    ! here we fix the basis to be a monoatomic square cell
-    basis%nAtom=1
-    basis%unitClength=1.0
-    allocate(basis%rBasis(1),basis%kBasis(1))
-    basis%rBasis%Comp=0.0d0
-    basis%rBasis%Norm=0.0d0
-    basis%kBasis%Comp=0.0d0
-    basis%kBasis%Norm=0.0d0
-  end subroutine getBasis
-    
+    !allocate fine grid and coarse grid sizes
 
-  subroutine getcoarseGrids(coarseGrid,ncell,unitClength)
-    ! dummy variables
-    integer::ncell
-    type(cellBasis),intent(inout)::coarseGrid
-    real(real12)::unitClength
+    !allocate k grid values
 
-    !routine variables
-    integer::ix,iy,iz
-    real::lx,ly,lz,px,py,pz
-    
-    coarseGrid%ncell=ncell
-    coarseGrid%nCelltot=ncell*ncell*ncell
-    coarseGrid%clusterLength=ncell*unitClength
-    
-    allocate(coarseGrid%rbasis(ncell,ncell,ncell))
-    allocate(coarseGrid%rbasis(ncell,ncell,ncell))
+    !allocate the coarse map points
 
-    do ix=1,ncell
-       px=2.0*pi*(real(ix-1,real12))/coarseGrid%clusterLength !0 to 2pi
-       px=px -pi/unitCLength+ 0.5d0*pi/coarseGrid%clusterLength !-pi to pi,
-                                                            !maps to cell centre
-       lx=2.0*pi/px
-       do iy=1,ncell
-          py=2.0*pi*(real(iy-1,real12))/coarseGrid%clusterLength !0 to 2pi
-          py=py -pi/unitCLength+ 0.5d0*pi/coarseGrid%clusterLength !-pi to pi,
-                                                            !maps to cell centre
-          ly=2.0*pi/py
-          do iz=1,ncell
-             py=2.0*pi*(real(iz-1,real12))/coarseGrid%clusterLength !0 to 2pi
-             pz=pz-pi/unitCLength+ 0.5d0*pi/coarseGrid%clusterLength !-pi to pi,
-                                                          !maps to cell centre
-             
-             coarseGrid%rbasis(ix,iy,iz)%Comp(1)=lx
-             coarseGrid%rbasis(ix,iy,iz)%Comp(2)=ly
-             coarseGrid%rbasis(ix,iy,iz)%Comp(3)=lz
-             coarseGrid%rbasis(ix,iy,iz)%Norm=sqrt(lx*lx&
-                  +ly*ly+lz*lz)
-             
-             coarseGrid%kbasis(ix,iy,iz)%Comp(1)=px
-             coarseGrid%kbasis(ix,iy,iz)%Comp(2)=py
-             coarseGrid%kbasis(ix,iy,iz)%Comp(3)=pz
-             coarseGrid%rbasis(ix,iy,iz)%Norm=sqrt(px*px&
-                  +py*py+pz*pz)
-          enddo
-       enddo
-    enddo
+    !calculate frequency squared for both grids
 
-  end subroutine getcoarseGrids
+  end subroutine initGrid
 
-  subroutine getfineGrids(finegrid,nfinecells,unitClength)
-    !dummy variables
-    
+  subroutine initGF()
+
+    !generate DO for fine grid points
+
+    !map to coarse grid
+
+  end subroutine initGF
+
+  subroutine initHybrid()
+
+    ! generate initial hybridisation
+
+  end subroutine initHybrid
+
     
 end module initialisation
-
